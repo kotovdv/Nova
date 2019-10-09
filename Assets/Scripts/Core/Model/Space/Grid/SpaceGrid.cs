bcdbd4f408b425947737894a31e1245174f4dd45
Dmitry Kotov <xeno93@gmail.com>
2019-10-09 22:51:50 +0300

    Reduce amount of calls to space tile cache during traversal

--- Assets/Scripts/Core/Model/Space/Grid/SpaceGrid.cs
+++ Assets/Scripts/Core/Model/Space/Grid/SpaceGrid.cs
@@ -27,6 +27,25 @@ namespace Core.Model.Space.Grid
         }
 
         /// <summary>
+        /// Executes given action for all the planets in given square
+        /// </summary>
+        /// <param name="square">Information about a square from the grid</param>
+        /// <param name="action">Action that will be performed on all planets during traversal.</param>
+        public void Traverse(Square square, IPlanetAction action)
+        {
+            for (var x = square.LeftX; x < square.LeftX + square.Size; x++)
+            for (var y = square.BottomY; y < square.BottomY + square.Size; y++)
+            {
+                var position = new Position(x, y);
+
+                var optPlanet = TryGetPlanet(position);
+                if (!optPlanet.HasValue) continue;
+
+                action.Invoke(position, optPlanet.Value);
+            }
+        }
+
+        /// <summary>
         /// Executes given action for each element at target location.
         /// Target location is determined via combination of square, side and offset.
         /// For example
@@ -58,33 +77,22 @@ namespace Core.Model.Space.Grid
             var finalX = (side == Side.Horizontal) ? initialX + square.Size : initialX + 1;
             var finalY = (side == Side.Vertical) ? initialY + square.Size : initialY + 1;
 
+            var cachedTilePosition = _navigator.FindTile(new Position(initialX, initialY));
+            var cachedTile = _spaceGridTileCache.Get(cachedTilePosition);
+
             for (var x = initialX; x < finalX; x++)
             for (var y = initialY; y < finalY; y++)
             {
                 var position = new Position(x, y);
+                var gridPosition = _navigator.Find(position);
+                if (!cachedTilePosition.Equals(gridPosition.TilePosition))
+                {
+                    cachedTilePosition = gridPosition.TilePosition;
+                    cachedTile = _spaceGridTileCache.Get(cachedTilePosition);
+                }
 
-                var optPlanet = TryGetPlanet(position);
+                var optPlanet = cachedTile.GetValue(gridPosition.ElementPosition);
                 if (!optPlanet.HasValue) continue;
-
-                action.Invoke(position, optPlanet.Value);
-            }
-        }
-
-        /// <summary>
-        /// Executes given action for all the planets in given square
-        /// </summary>
-        /// <param name="square">Information about a square from the grid</param>
-        /// <param name="action">Action that will be performed on all planets during traversal.</param>
-        public void Traverse(Square square, IPlanetAction action)
-        {
-            for (var x = square.LeftX; x < square.LeftX + square.Size; x++)
-            for (var y = square.BottomY; y < square.BottomY + square.Size; y++)
-            {
-                var position = new Position(x, y);
-
-                var optPlanet = TryGetPlanet(position);
-                if (!optPlanet.HasValue) continue;
-
                 action.Invoke(position, optPlanet.Value);
             }
         }
